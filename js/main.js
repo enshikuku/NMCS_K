fe730c77e22b82586d36c8bc47bec99736780f6e
Shii_039 <enshikuku39@gmail.com>
2022-08-22 10:36:59 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 const load = document.querySelector('.loader')
-let body = document.querySelector('.body')
 window.addEventListener("load", function(){
     load.style.display = 'none'
-    body.style.display = 'block'
 })
