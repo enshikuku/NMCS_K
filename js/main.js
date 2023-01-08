27f0495f7d0e1e31b40d81522886a14fc253a1fd
Shikuku39 <enhikuku39@gmail.com>
2023-01-08 17:58:57 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 const load = document.querySelector('.loader')
-window.addEventListener("load", function(){
-    load.style.display = 'none'
-})
 mybutton = document.querySelector('#scrollTopBtn')
 
-// When the user scrolls down 50px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()}
+window.addEventListener("load", function() {
+    load.style.display = 'none'
+})
+
+window.onscroll = function() {
+    scrollFunction()
+}
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block"
-  } else {
-    mybutton.style.display = "none"
-  }
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block"
+    } else {
+        mybutton.style.display = "none"
+    }
 }
+
 function topFunction() {
-  document.body.scrollTop = 0 // For Safari
-  document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0 // For Safari
+    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
